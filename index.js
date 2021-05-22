2fc6cf2fbbc40815bed26d346158769d75b225f8
Gift Enyoghasim <73113145+enyoghasim@users.noreply.github.com>
2021-05-22 15:35:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { resolve } = require("path");
 
 const app = express();
-
+app.set("trust proxy", 1);
 app.use(
   express.static(resolve(__dirname, "..", "the-gainer-frontend", "dist"))
 );
